811675c3194ddf651303fdc8609fa6fa2cf7550c
marcosignacio <marcos.huenchumil@ug.uchile.cl>
2025-06-25 01:32:05 -0400

    init branch

--- src/loader.py
+++ src/loader.py
@@ -1,15 +1,34 @@
 import os
 import pandas as pd
 from typing import Any
+from sklearn.model_selection import train_test_split
+
+SEED = 42
+
+
+def split_temporal(
+    df, target_col, test_size=0.1, val_size=0.2, sort_by="purchase_date", seed=SEED
+):
+    df_sorted = df.sort_values(by=sort_by)
+    X = df_sorted.drop(columns=[target_col])
+    y = df_sorted[target_col]
+
+    # Separar test
+    X_temp, X_test, y_temp, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=seed, shuffle=False
+    )
+
+    # Separar validation del restante
+    val_prop = val_size / (1 - test_size)
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_temp, y_temp, test_size=val_prop, random_state=seed, shuffle=False
+    )
+    return X_train, X_val, X_test, y_train, y_val, y_test
 
 
 def split_data(ds: str, **kwargs: Any) -> None:
     ti = kwargs["ti"]
-    paths = ti.xcom_pull(key="paths", task_ids="create_folders")
+    dfs = ti.xcom_pull(key="datasets", task_ids="ingestion_data")
 
-    raw_data_path = os.path.join(paths["raw_data"], "data.csv")
-
-    df = pd.read_csv(raw_data_path)
-
-    # Aquí iría la lógica para separar entrenamiento/validación
-    print("Splitting data into training and validation sets...")
+    for name, df in dfs.items():
+        pass
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,14 @@
 import os
-from typing import Any
-import pandas as pd
 import shutil
 
 
-def creates_folders(ds: str, **kwargs: Any) -> None:
-    """ " Crea las carpetas necesarias para almacenar los datos y modelos de la iteración actual"""
+def creates_folders(ds: str, **kwargs) -> None:
+    """
+    Crea las carpetas necesarias para almacenar los datos y modelos de la iteración actual
+    """
+
     BASE_PATH = os.path.join(os.getenv("AIRFLOW_HOME", "."), "runs", ds)
+    print(f"Creating folders in {BASE_PATH} for date {ds}")
 
     subfolders = [
         "raw_data",
@@ -25,37 +27,25 @@ def creates_folders(ds: str, **kwargs: Any) -> None:
     kwargs["ti"].xcom_push(key="paths", value=paths)
 
 
-def ingestion_data(ds: str, **kwargs: Any) -> None:
-    """Agrega los datasets a la carpeta raw_data  y los guarda en un diccionario"""
+# emula la funcion de download de los datasets y los guarda en la carpeta raw_data
+def ingestion_data(ds: str, **kwargs) -> None:
+    """
+    Copia y mergea los .parquet de data/ en raw_data
+    """
+
     ti = kwargs["ti"]
     paths = ti.xcom_pull(key="paths", task_ids="create_folders")
-    dataset_names = os.listdir("data")
 
-    # se hace una copia de los datasets a la carpeta raw_data para la k-esima iteración
-    src_path = [
-        os.path.join("data", name)
-        for name in dataset_names
-        if name.endswith(".parquet")
+    parquet_files = [f for f in os.listdir("data") if f.endswith(".parquet")]
+
+    [
+        shutil.copy(os.path.join("data", f), os.path.join(paths["raw_data"], f))
+        for f in parquet_files
+        if not os.path.exists(os.path.join(paths["raw_data"], f))
     ]
 
-    dest_path = [
-        os.path.join(paths["raw_data"], os.path.basename(name)) for name in src_path
-    ]
-
-    datasets = dict()
-    for src, dest in zip(src_path, dest_path):
-        if os.path.exists(dest):
-            print(f"File {dest} already exists. Skipping copy.")
-            continue
-
-        # se copia el dataset a la carpeta raw_data
-        print(f"Copying {src} to {dest}...")
-        shutil.copy(src, dest)
-
-        # se leen los datos y se guardan en un Dataframe
-        df = pd.read_parquet(dest)
-        datasets[os.path.basename(dest)] = df
-
-    kwargs["ti"].xcom_push(key="datasets", value=datasets)
-
-    print(f"Datasets ingested: {list(datasets.keys())}")
+    # pasarr la ruta de los datasets a xcom . no usar read_parquet
+    ti.xcom_push(
+        key="datasets",
+        value={f: os.path.join(paths["raw_data"], f) for f in parquet_files},
+    )
