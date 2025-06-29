c2908a0dcb20aedae1462afe4a31123998889628
marcosignacio <marcos.huenchumil@ug.uchile.cl>
2025-06-29 03:40:20 -0400

    pipeline

--- main_dag.py
+++ main_dag.py
@@ -1,17 +1,54 @@
 from src.preprocessing import preprocessing_data
+from src.utils import creates_folders, ingestion_data
+from src.loader import split_data
 import pandas as pd
 
 
 df = pd.read_parquet("data/transacciones.parquet")
 
+
+ds = "13132024"
+df_completed_path = "data/df_completed.parquet"
+
 data_path = {
-    "clientes": "data/clientes.parquet",
-    "transacciones": "data/transacciones.parquet",
-    "productos": "data/productos.parquet",
+    "raw_data": "runs/13132024/raw_data",
+    "preprocessed_data": "runs/13132024/preprocessed_data",
+    "splits_data": "runs/13132024/splits_data",
+    "models": "runs/13132024/models",
 }
 
-preprocessing_data_ = preprocessing_data(
-    ds="{{ ds }}",
+datasets = {
+    "clientes": "runs/13132024/raw_data/clientes.parquet",
+    "transacciones": "runs/13132024/raw_data/transacciones.parquet",
+    "productos": "runs/13132024/raw_data/productos.parquet",
+}
+
+df_completed_path = {
+    "df_completed_path": "runs/13132024/preprocessed_data/df_completed.parquet",
+}
+
+task_creates_folders = creates_folders(
+    ds=ds,
+)
+
+task_ingestion_data = ingestion_data(
+    ds=ds,
     data_path=data_path,
+    dev=True,  # Set to True for development mode
+)
+
+
+preprocessing_data_ = preprocessing_data(
+    ds=ds,
+    data_path=datasets,
+    paths=data_path,
+    dev=True,  # Set to True for development mode
+)
+
+
+split_data_ = split_data(
+    ds=ds,
+    data_completed_path=df_completed_path,
+    paths=data_path,
     dev=True,  # Set to True for development mode
 )
--- src/loader.py
+++ src/loader.py
@@ -9,7 +9,7 @@ SEED = 42
 def split_temporal(
     df, target_col, test_size=0.1, val_size=0.2, sort_by="purchase_date", seed=SEED
 ):
-    df_sorted = df.sort_values(by=sort_by)
+    df_sorted = df.sort_values(by=["year", "week"], ascending=True)
     X = df_sorted.drop(columns=[target_col])
     y = df_sorted[target_col]
 
@@ -26,9 +26,37 @@ def split_temporal(
     return X_train, X_val, X_test, y_train, y_val, y_test
 
 
-def split_data(ds: str, **kwargs: Any) -> None:
-    ti = kwargs["ti"]
-    dfs = ti.xcom_pull(key="datasets", task_ids="ingestion_data")
+def split_data(ds: str, **kwargs) -> None:
+    if not kwargs["dev"]:
+        ti = kwargs["ti"]
+        df_completed_path = ti.xcom_pull(
+            key="df_completed_path", task_ids="preprocessing_data"
+        )
+    df_completed_path = kwargs.get("data_completed_path")["df_completed_path"]
+    preprocessed_path = kwargs.get("paths")["splits_data"]
+    df = pd.read_parquet(df_completed_path)
 
-    for name, df in dfs.items():
-        pass
+    X_train, X_val, X_test, y_train, y_val, y_test = split_temporal(
+        df,
+        target_col="target",
+        test_size=0.1,
+        val_size=0.2,
+        sort_by="purchase_date",
+        seed=SEED,
+    )
+
+    # Guardar los splits
+    X_train.to_parquet(os.path.join(preprocessed_path, "X_train.parquet"), index=False)
+    X_val.to_parquet(os.path.join(preprocessed_path, "X_val.parquet"), index=False)
+    X_test.to_parquet(os.path.join(preprocessed_path, "X_test.parquet"), index=False)
+
+    print(f"informacion X_train: {X_train.shape} columnas: {X_train.columns} \n")
+    y_train.to_frame().to_parquet(
+        os.path.join(preprocessed_path, "y_train.parquet"), index=False
+    )
+    y_val.to_frame().to_parquet(
+        os.path.join(preprocessed_path, "y_val.parquet"), index=False
+    )
+    y_test.to_frame().to_parquet(
+        os.path.join(preprocessed_path, "y_test.parquet"), index=False
+    )
